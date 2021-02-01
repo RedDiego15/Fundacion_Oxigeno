e0210ea500708713fbeed3e449a92ec22058ab51
Diego Rojas <Diego-27ro@outlook.com>
2021-02-01 17:03:53 -0500

    intentento hacer post

--- fundation_oxygen/app/controllers/home_controller.rb
+++ fundation_oxygen/app/controllers/home_controller.rb
@@ -5,19 +5,25 @@ class HomeController < ApplicationController
   before_action :set_dato, only: [:show, :edit, :update, :destroy]
 
   def index
-    @response = HTTParty.get('http://localhost:8000/api/supplys')
+    @response = HTTParty.get('https://oxigenfundationapi.herokuapp.com/api/supplys')
   end
 
   def realizarDonacion
-    
+
+
+    def realizar_Donacion(name,id_property,amount)
+    @send = HTTParty.post('https://oxigenfundationapi.herokuapp.com/api/supplys',
+                        
+                          :query => {'name': name,
+                                      'id_property':id_property,
+                                      'amount': amount});
+    end
   end
 
   # Este metodo se llama automaticamente en la vista home/solicitarDonacion
   def solicitarDonacion
-    @supplys = HTTParty.get('http://localhost:8000/api/supplys')
-    @oxigens = HTTParty.get('http://localhost:8000/api/oxigens')  
+    @supplys = HTTParty.get('https://oxigenfundationapi.herokuapp.com/api/supplys')
+    @oxigens = HTTParty.get('https://oxigenfundationapi.herokuapp.com/api/oxigens')  
   end
-
-
 
 end
