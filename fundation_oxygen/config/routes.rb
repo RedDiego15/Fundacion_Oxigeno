031cfacb2482f61b5f0a4211773354e97986d005
Gary Barzola <gnbarzol@MacBook-Pro-de-Gary.local>
2021-02-02 16:52:25 -0500

    update routes

--- fundation_oxygen/config/routes.rb
+++ fundation_oxygen/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
-  get   'home/realizarDonacion'
-  get   'home/solicitarDonacion'
-  # get   'donations/donation'
+  get   'realizarDonacion', to: 'home/realizarDonacion'
+  get   'solicitarDonacion', to: 'home/solicitarDonacion'
+
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
